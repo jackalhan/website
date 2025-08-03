2f2de266e4be62215766a476368cf25122bc5766
jackalhan <jackalhan@users.noreply.github.com>
2025-08-03 19:28:24 +0000

    Deploying to gh-pages from @ jackalhan/website@301b54c5eaf511571262d66b539488a15430e321 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,13 +466,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/tolgahan", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=OvgwYg0AAAAJ&hl", "_blank");
-        },
-      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
